80a073ac676ace0aba74957a2cd7b13751255c5e
k1nd0ne <k1nd0ne@mail.com>
2024-12-22 12:39:12 +0100

    Dependencies and lib cleaning going on

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub mod ewf;
+pub mod raw;
--- src/raw/mod.rs
+++ src/raw/mod.rs
@@ -1,24 +1,27 @@
-use std::{path::{Path}, fs::File, io::{Read, Seek, SeekFrom}};
-pub struct RAW{
+use std::{
+    fs::File,
+    io::{Read, Seek, SeekFrom},
+    path::Path,
+};
+pub struct RAW {
     image: File,
     _description: String,
 }
 
-impl RAW{
-    pub fn new(file_path: &str) -> Result<RAW, String>{
+impl RAW {
+    pub fn new(file_path: &str) -> Result<RAW, String> {
         let file: &Path = Path::new(file_path);
-        
-        let fd = match File::open(file){
+
+        let fd = match File::open(file) {
             Ok(file) => file,
-            Err(m) => return Err(m.to_string())
+            Err(m) => return Err(m.to_string()),
         };
 
-        return Ok(RAW{
+        return Ok(RAW {
             image: fd,
             _description: "RAW image format".to_string(),
-        })
+        });
     }
-
 
     pub fn read(&mut self, size: usize) -> Vec<u8> {
         // Read data into the buffer in chunks
@@ -27,7 +30,7 @@ impl RAW{
         return buffer;
     }
 
-    pub fn seek(&mut self, offset: usize){
+    pub fn seek(&mut self, offset: usize) {
         self.image.seek(SeekFrom::Start(offset as u64)).unwrap();
     }
 }
